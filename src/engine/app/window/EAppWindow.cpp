8a504804b05acdd0339d27492cb804315fb00eeb
Vadim Zhilinkov <dreadct@gmail.com>
2025-10-23 13:13:50 +0700

    Add and use a new EAppWindow class
    For initializing the app's window.

---
+++ src/engine/app/window/EAppWindow.cpp
@@ -0,0 +1,77 @@
+#include "EAppWindow.h"
+
+#include <stdexcept>
+#include <utility>
+
+// Class lifecycle
+
+EAppWindow::EAppWindow(
+    const std::string& title,
+    const uint32_t width,
+    const uint32_t height
+) {
+    if ((window = SDL_CreateWindow(
+        title.c_str(),
+        (int)width,
+        (int)height,
+        0
+    )) == nullptr) {
+        std::string error = "Failed to init SDL window: ";
+        error += SDL_GetError();
+        throw std::runtime_error(error);
+    }
+}
+
+EAppWindow::~EAppWindow() {
+    if (window) {
+        SDL_DestroyWindow(window);
+    }
+}
+
+EAppWindow::EAppWindow(
+    EAppWindow&& other
+) noexcept : window(std::exchange(other.window, nullptr)) {
+}
+
+EAppWindow& EAppWindow::operator=(
+    EAppWindow&& other
+) noexcept {
+    window = std::exchange(other.window, nullptr);
+    return *this;
+}
+
+// Modifying window
+
+std::string EAppWindow::getTitle() const {
+    return SDL_GetWindowTitle(window);
+}
+
+void EAppWindow::setTitle(const std::string& title) {
+    SDL_SetWindowTitle(window, title.c_str());
+}
+
+std::tuple<uint32_t, uint32_t> EAppWindow::getSize() const {
+    int width = 0, height = 0;
+    SDL_GetWindowSize(window, &width, &height);
+    return { (uint32_t)width, (uint32_t)height };
+}
+
+void EAppWindow::setSize(
+    const uint32_t width,
+    const uint32_t height
+) {
+    SDL_SetWindowSize(window, (int)width, (int)height);
+}
+
+bool EAppWindow::hide() {
+    return SDL_HideWindow(window);
+}
+
+bool EAppWindow::isHidden() const {
+    auto flags = SDL_GetWindowFlags(window);
+    return (flags & SDL_WINDOW_HIDDEN) != 0;
+}
+
+bool EAppWindow::show() {
+    return SDL_ShowWindow(window);
+}
